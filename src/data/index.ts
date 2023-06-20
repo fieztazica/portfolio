e6e17a4adb1b923c7373afd3822342aa07092120
fiezt <dat09t@gmail.com>
2023-06-20 12:35:47 +0700

    test astro build

---
+++ src/data/index.ts
@@ -0,0 +1,30 @@
+export type Social = {
+    name: string,
+    icon: string,
+    href: string
+}
+
+const socials: Social[] = [
+    {
+        name: "GitHub",
+        icon: "mdi:github",
+        href: "https://github.com/fiezt1492"
+    },
+    {
+        name: "LinkedIn",
+        icon: "mdi:linkedin",
+        href: "https://www.linkedin.com/in/fiezt/"
+    },
+    {
+        name: "Instagram",
+        icon: "mdi:instagram",
+        href: "https://instagram.com/fieztazica"
+    },
+    {
+        name: "Discord",
+        icon: "ic:baseline-discord",
+        href: "https://discord.gg/F7ZK6ssMUm"
+    },
+]
+
+export { socials }
