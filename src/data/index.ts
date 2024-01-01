9032cae003718295006153b6016f1896be57bf1f
Dat Hoang <fiezt@outlook.com>
2024-01-01 13:48:04 +0700

    update astro, ui

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,30 +1,44 @@
-export type Social = {
-    name: string,
-    icon: string,
+export type Href = {
+    name: string
     href: string
 }
 
-const socials: Social[] = [
+export type HrefIcon = Href & {
+    icon: string
+}
+
+const socials: HrefIcon[] = [
     {
-        name: "GitHub",
-        icon: "mdi:github",
-        href: "https://github.com/fiezt1492"
+        name: 'GitHub',
+        icon: 'mdi:github',
+        href: 'https://github.com/fiezt1492',
     },
     {
-        name: "LinkedIn",
-        icon: "mdi:linkedin",
-        href: "https://www.linkedin.com/in/fiezt/"
+        name: 'LinkedIn',
+        icon: 'mdi:linkedin',
+        href: 'https://www.linkedin.com/in/fiezt/',
     },
     {
-        name: "Instagram",
-        icon: "mdi:instagram",
-        href: "https://instagram.com/fieztazica"
+        name: 'Instagram',
+        icon: 'mdi:instagram',
+        href: 'https://instagram.com/fieztazica',
     },
     {
-        name: "Discord",
-        icon: "ic:baseline-discord",
-        href: "https://discord.com/users/445102575314927617"
+        name: 'Discord',
+        icon: 'ic:baseline-discord',
+        href: 'https://discord.com/users/445102575314927617',
     },
 ]
 
-export { socials }
+const navLinks: Href[] = [
+    {
+        name: 'About',
+        href: '/about',
+    },
+    {
+        name: 'Contact',
+        href: '/contact',
+    },
+]
+
+export { socials, navLinks }
