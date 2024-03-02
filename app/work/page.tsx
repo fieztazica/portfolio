458d0fc7d8c3105acc735593e977b58c4b4f95d7
Dat Hoang <fiezt@outlook.com>
2024-03-02 09:45:52 +0700

    restyle work page

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -6,6 +6,26 @@ export const metadata: Metadata = {
     description: 'A summary of my work and contributions.',
 }
 
+function GitHubLink({ link }) {
+    return (
+        <a
+            className="group flex items-center hover:text-neutral-800 dark:hover:text-neutral-100 transition-all"
+            rel="noopener noreferrer"
+            target="_blank"
+            href={link}
+        >
+            <div className="text-neutral-700 dark:text-neutral-300 transform transition-transform duration-300 group-hover:-rotate-12 rounded-full bg-neutral-700 dark:bg-neutral-300 p-1 h-fit">
+                <img
+                    alt="GitHub logo"
+                    src="/github-logo.svg"
+                    width="32"
+                    height="32"
+                />
+            </div>
+        </a>
+    )
+}
+
 export default function Page() {
     return (
         <section>
@@ -18,24 +38,53 @@ export default function Page() {
                     for internship or fresher position.
                 </p>
                 <Hr />
-                <h2 className="font-medium text-xl mb-1 tracking-tighter">
-                    Code MeLy
-                </h2>
-                <p className="text-neutral-600 dark:text-neutral-400 text-sm">
-                    Technical Member, Aug 2022 — Dec 2023
-                </p>
+                <div className="flex justify-between items-center">
+                    <div>
+                        <h2 className="font-medium text-xl mb-1 tracking-tighter">
+                            Code MeLy
+                        </h2>
+                        <p className="text-neutral-600 dark:text-neutral-400 text-sm">
+                            Technical Member, Aug 2022 — Dec 2023
+                        </p>
+                    </div>
+                    <GitHubLink link={'https://github.com/mely-apps'} />
+                </div>
                 <p>
-                    <a href="https://github.com/mely-apps">Code MeLy</a> is a
-                    GenZ IT/Dev community in Vietnam. I joined early and help
-                    them to build community. Before the time I joined, Code MeLy
-                    was known as IT Town but its co-founders have decided to
-                    renew their brand to attract the young IT generation. I
-                    participated in as their Discord Server Administrator and
-                    help them to setup, build a chat bot, grow{' '}
-                    <a href="https://discord.gg/codemely">Discord Server</a>{' '}
-                    community besides their Facebook{' '}
-                    <a href="https://www.facebook.com/code.mely">page</a> and{' '}
-                    <a href="https://www.facebook.com/groups/devoiminhdidauthe">
+                    <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://www.linkedin.com/company/code-mely"
+                    >
+                        Code MeLy
+                    </a>{' '}
+                    is a GenZ IT/Dev community in Vietnam. I joined early and
+                    helped them to build community. Before the time I joined,
+                    Code MeLy was known as IT Town but its co-founders have
+                    decided to renew their brand to attract the young IT
+                    generation. I participated in as their Discord Server
+                    Administrator and helped them to setup, built a chat bot,
+                    grew{' '}
+                    <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://discord.gg/codemely"
+                    >
+                        Discord Server
+                    </a>{' '}
+                    community beside their Facebook{' '}
+                    <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://www.facebook.com/code.mely"
+                    >
+                        page
+                    </a>{' '}
+                    and{' '}
+                    <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://www.facebook.com/groups/devoiminhdidauthe"
+                    >
                         group
                     </a>
                     .
@@ -46,14 +95,21 @@ export default function Page() {
                     still moderate the Discord server as Administrator.`}
                 </p>
                 <Hr />
-                <h2 className="font-medium text-xl mb-1 tracking-tighter">
-                    ProfioApp
-                </h2>
-                <p className="text-neutral-600 dark:text-neutral-400 text-sm">
-                    Frontend, Aug 2023 — Nov 2023
-                </p>
+                <div className="flex justify-between items-center">
+                    <div>
+                        <h2 className="font-medium text-xl mb-1 tracking-tighter">
+                            ProfioApp
+                        </h2>
+                        <p className="text-neutral-600 dark:text-neutral-400 text-sm">
+                            Frontend, Aug 2023 — Nov 2023
+                        </p>
+                    </div>
+                    <GitHubLink
+                        link={'https://github.com/HutechCJ/ProfioApp'}
+                    />
+                </div>
                 <p>
-                    We (5 members) did this project for an IT Contest{' '}
+                    We (5 members) finished this project for an IT Contest{' '}
                     <a href="https://www.linkedin.com/posts/cj-olivenetworks-vina_itcontest-codeabryourabrfuture-cjvietnamhq-activity-7117777134567256067-CB5n">
                         Code Your Future
                     </a>{' '}
@@ -63,16 +119,23 @@ export default function Page() {
                     </a>{' '}
                     in just 3 months and rewarded the 'real' third prize.{' '}
                     {`My job
-                    is to handle the CMS/Dashboard which is written with Next.js of this
+                    is to handle the CMS/Dashboard which is written with Next.js for this
                     project.`}
                 </p>
                 <Hr />
-                <h2 className="font-medium text-xl mb-1 tracking-tighter">
-                    Vietnamese Chess
-                </h2>
-                <p className="text-neutral-600 dark:text-neutral-400 text-sm">
-                    Frontend, Sep 2023 — Now
-                </p>
+                <div className="flex justify-between items-center">
+                    <div>
+                        <h2 className="font-medium text-xl mb-1 tracking-tighter">
+                            Vietnamese Chess
+                        </h2>
+                        <p className="text-neutral-600 dark:text-neutral-400 text-sm">
+                            Frontend, Sep 2023 — Now
+                        </p>
+                    </div>
+                    <GitHubLink
+                        link={'https://github.com/orgs/Owlvernyte/projects/1'}
+                    />
+                </div>
                 <p>
                     An Owlvernyte team's school project (7 members).{' '}
                     <a href="https://cotuong-nyte.vercel.app">
